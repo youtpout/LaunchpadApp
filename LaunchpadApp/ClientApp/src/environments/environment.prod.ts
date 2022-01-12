d522af7db0e9459e5fac782277859e7b1631b4ae
Eddy Boughioul <eddyboughioul@hotmail.fr>
2022-01-12 14:20:22 +0100

    update config for deployment

--- LaunchpadApp/ClientApp/src/environments/environment.prod.ts
+++ LaunchpadApp/ClientApp/src/environments/environment.prod.ts
@@ -1,8 +1,15 @@
 export const environment = {
   production: true,
-  creatorAddress: '0x10d815C2d90aF0858dcCD3c265BED0D486239e7c',
-  storageAddress: '0x674A90e6bc29ef188c98d85A681300bEFA162110',
+  //creatorAddress: '0x10d815C2d90aF0858dcCD3c265BED0D486239e7c',
+  //storageAddress: '0x674A90e6bc29ef188c98d85A681300bEFA162110',
+  //pancakeRouter: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
+  //chainId: '0x38',
+  //explorer: 'https://bscscan.com/token/'
+  creatorAddress: '0xFB8873c492BeAc6e328C4378aE35409F848c7F25',
+  storageAddress: '0xca923f38d8f444DF6950bfaDcb7EB183Ea803719',
   pancakeRouter: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
-  chainId: '0x38',
-  explorer: 'https://bscscan.com/token/'
+  launchpadAddress: '0xFd553404A262FEb94c08FCe4b086A212BA5F4efF',
+  chainId: '0x61',
+  explorer: 'https://testnet.bscscan.com/address/'
+
 };
